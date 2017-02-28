3bf644a59cf557e06daf71672decde256a680193
Humakt83 <urlact@yahoo.com>
2017-02-28 23:04:55 +0200

    Home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { quotes } from './quotes';
 
 @Component({
     selector: 'home',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+    quote: string = this.pickRandomQuote();
+
+    private pickRandomQuote(): string {
+        return quotes[Math.floor(Math.random() * quotes.length)];
+    }    
 }
---
+++ src/app/home/quotes.ts
@@ -0,0 +1,11 @@
+export const quotes: string[] = [
+    "Big peg-legged pig in the bog begs for bag",
+    "Witness the fitless fitness of the witless!",
+    "Storms are unpredictable. So am I!",
+    "Onpa se vaikeaa. Yritä sinä.",
+    "You eat plants, I eat plants. You eat monsters, I eat you. -Troll",
+    "Death must be impartial. I must sever my ties, lest I shield my kin.",
+    "Don't laugh, it works.",
+    "Min pepperplants! Dån't fårget min pepperplants!",
+    "Thine swine whine when it drank all wine of mine and was strangled with vine."
+];
